df5cbb6fcb9eafbc333042861f0ac010c84838c5
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-05-09 14:06:45 +0100

    multiple html pages and css

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.listen(5001, () => {
+    console.log(`Listening on port 5001`)
+})
+
+
+
+
